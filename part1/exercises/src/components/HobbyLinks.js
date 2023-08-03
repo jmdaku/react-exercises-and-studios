b2c07d6aa2c41d4d298f188f10b1cf472f36c5e0
jmdaku <jmdaku@gmail.com>
2023-08-02 22:03:41 -0400

    completed part 2 & 3

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,13 @@
+
+export default function HobbyLinks() {
+    
+        const hobbyLinks = ["https://researchingfoodhistory.blogspot.com/2017/09/seckel-or-seckle-pears-originally-from.html", "https://www.aracari.com/blog/stories/a-guide-to-peruvian-potatoes/"];
+            return (
+                <ul>
+                 <li><a href = {hobbyLinks[0]}>Researching Food History: Seckel or Seckle Pears originally from Philadelphia</a></li>
+                 <li><a href = {hobbyLinks[0]}>A Guide To Peruvian Potatoes</a></li>
+                </ul>            
+            );
+   
+}
+

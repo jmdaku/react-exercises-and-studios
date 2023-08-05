d53373b2e1b4e8ad3f5ccd205d757f369ede2a08
jmdaku <jmdaku@gmail.com>
2023-08-05 16:41:12 -0400

    added recipe photo & related css edits

--- part1/studio 8/src/App.js
+++ part1/studio 8/src/App.js
@@ -2,16 +2,21 @@
 import './App.css';
 import RecipeDescription from './components/Description.js';
 import RecipeIngredients from './components/Ingredients.js';
+import RecipePhoto from './components/Photos.js';
 
 
 function App() {
   return (
-    
-      <div>
-      <RecipeDescription />
-      <RecipeIngredients />
+    <div className="App">
+   <div>
+      <RecipePhoto className="recipePhotoBlock"/>
+      
+      <div className="recipePhotoBlock">
+         <RecipeDescription />
+         <RecipeIngredients />
       </div>
-    
+   </div>
+ </div>
   );
 }
 
---
+++ part1/studio 8/src/components/Photos.js
@@ -0,0 +1,10 @@
+import styles from './Description.module.css';
+
+const RecipePhoto = () => {
+
+    return(
+        <img src="https://www.foodandwine.com/thmb/yK_YXkAkPH8VN1W1UGt60pfDCeg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/201010-xl-german-soft-pretzel-sticks-2000-414b59d2f78b4a1eb07a878212d2b160.jpg" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
+
+export default RecipePhoto;

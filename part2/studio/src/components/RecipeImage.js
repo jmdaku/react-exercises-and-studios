d599011e44b4eb76f7c4903ee59592327410111d
jmdaku <jmdaku@gmail.com>
2023-08-07 21:37:28 -0400

    added ingrediants

--- part2/studio/src/components/RecipeImage.js
+++ part2/studio/src/components/RecipeImage.js
@@ -5,10 +5,12 @@ function RecipeImage() {
   const recipeImage = recipedata.map((data) => (
      <div key={data.name}>
      <img src={data.recipeImage} alt={data.name} className="recipeImage" />
-    </div>
+     </div>
   )
   )
-   return RecipeImage;
+  return (
+    <div>{recipeImage} </div>
+  )
  }
  
  export default RecipeImage;
--- part2/studio/src/components/RecipeName.js
+++ part2/studio/src/components/RecipeName.js
@@ -1,17 +1,17 @@
 import recipedata from "./recipe.json";
 
 
-const props = recipedata
-
 function RecipeName() {
   const recipeName = recipedata.map((data) => (
-    <h1>
       <div key={data.name}>
       </div>
-    </h1> 
-  )
-  )
-    return RecipeName;
+      )
+      )
+    return (
+      <h1>
+      {recipeName}
+      </h1>
+    )  
 }
 
 export default RecipeName;

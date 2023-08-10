008100d607c8992269dbbc51d1cee2b0a2ecfa27
jmdaku <jmdaku@gmail.com>
2023-08-09 20:39:38 -0400

    started exercises pt 3

---
+++ part3/my-app/src/components/Introduction.js
@@ -0,0 +1,18 @@
+
+
+
+const HobbyIntroduction = () => {
+    return(
+        <div>
+           <h2>3 Fun Facts About this Hobby</h2>
+           <ol>
+                <li>Melons, cucumbers and squash are all closely related.</li>
+                <li>Potato plants are usually grown from existing potatoes but they can also be grown from seed with unpredictable results.</li>
+                <li>Almost all fruit trees are grafted and not grown from seed.</li> 
+            </ol>            
+        </div>
+    );
+ }
+
+
+ export default HobbyIntroduction;
